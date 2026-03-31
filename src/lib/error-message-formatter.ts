4b15574d59da20ce09d8a9d5be80213a8d94ea00
Eric Masseran <rico.masseran@gmail.com>
2026-03-31 21:25:16 +0200

    Drop error escape

--- src/lib/error-message-formatter.ts
+++ src/lib/error-message-formatter.ts
@@ -1,25 +1,5 @@
-const REPLACE_PAIRS = [
-  ["\n", "\\n"],
-  ["*", "\\*"],
-  ["_", "\\_"],
-  ["[", "\\["],
-  ["]", "\\]"],
-];
-
 export class ErrorMessageFormatter {
   format(message: string) {
-    const trimmedMessage = (message || "").trim();
-    return this.escapeText(trimmedMessage);
+    return (message || "").trim();
   }
-
-  private escapeText(string: string) {
-    return REPLACE_PAIRS.reduce(
-      (s, pair) => replaceAll(s, pair[0], pair[1]),
-      string,
-    );
-  }
-}
-
-function replaceAll(string: string, fromStr: string, toStr: string) {
-  return string.split(fromStr).join(toStr);
 }
--- src/test/lib/error-message-formatter.test.ts
+++ src/test/lib/error-message-formatter.test.ts
@@ -4,33 +4,8 @@ import { ErrorMessageFormatter } from "../../lib/error-message-formatter";
 describe("ErrorMessageFormatter", () => {
   const formatter = new ErrorMessageFormatter();
 
-  it("pass through normal text", () => {
-    const formattedText = formatter.format("normal text");
+  it("trim text", () => {
+    const formattedText = formatter.format("  normal text  ");
     assert.deepStrictEqual(formattedText, "normal text");
-  });
-
-  it("escape newline characters to show all lines in one line", () => {
-    const formattedText = formatter.format("MESSAGE\nCONTAINS\nNEWLINES\n");
-    assert.deepStrictEqual(formattedText, "MESSAGE\\nCONTAINS\\nNEWLINES");
-  });
-
-  it("escape `*` character", () => {
-    const formattedText = formatter.format("**bold**");
-    assert.deepStrictEqual(formattedText, "\\*\\*bold\\*\\*");
-  });
-
-  it("escape `_` character", () => {
-    const formattedText = formatter.format("__italic__");
-    assert.deepStrictEqual(formattedText, "\\_\\_italic\\_\\_");
-  });
-
-  it("escape `[` character", () => {
-    const formattedText = formatter.format("[[");
-    assert.deepStrictEqual(formattedText, "\\[\\[");
-  });
-
-  it("escape `]` character", () => {
-    const formattedText = formatter.format("]]");
-    assert.deepStrictEqual(formattedText, "\\]\\]");
   });
 });
